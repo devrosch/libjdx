da981c15b0f6a03338fbc3dadc8125b62b4aca94
devrosch <devrosch@gmx.net>
2023-04-09 13:52:34 +0200

    rename targets

--- apps/Main.cpp
+++ apps/Main.cpp
@@ -4,6 +4,6 @@ int main()
 {
     std::cout << "C++: main() executed\n";
     // noop, required for emscripten, so that *.js and *.wasm files get
-    // geenrated for library configured as dependency see:
+    // generated for library configured as dependency see:
     // https://stackoverflow.com/questions/34234446/cmake-is-it-possible-to-build-an-executable-from-only-static-libraries-and-no-s
 }
---
+++ src/sf/Wrapper.cpp
@@ -0,0 +1,1 @@
+// just a dummy file for cmake configure tzo work
