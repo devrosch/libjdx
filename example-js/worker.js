c0e728efd3de6d796ed2a94fac85be6a64e8d4ad
devrosch <devrosch@gmx.net>
2023-04-11 08:07:50 +0200

    move and fix js wasm example code

--- example-js/worker.js
+++ example-js/worker.js
@@ -1,12 +1,19 @@
 onmessage = function(e) {
     const f = e.data[0];
 
-    FS.mkdir('/work');
-    FS.mount(WORKERFS, { files: [f] }, '/work');
+    const dir = '/work';
+    const dirExists = FS.analyzePath(dir, false).exists;
+    if (!dirExists) {
+        FS.mkdir(dir);
+    }
+    FS.mount(WORKERFS, { files: [f] }, dir);
 
     let sfr = new Module.StubFileParser();
     //console.log('JS (Service Worker): "/work/' + f.name + '" found: ' + sfr.isResponsible('/work/' + f.name));
     console.log('JS (Service Worker): "/work/' + f.name + '" found: ' + sfr.isRecognized('/work/' + f.name));
+    sfr.delete();
+
+    FS.unmount(dir);
 }
 
 self.importScripts('libsf.js');
