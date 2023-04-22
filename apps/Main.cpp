84e8287fac4fec3b373c58e00761ac64e321dcaa
devrosch <devrosch@gmx.net>
2023-04-22 10:57:12 +0300

    print library name/version when loading wasm lib

--- apps/Main.cpp
+++ apps/Main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+#ifdef SF_PROJECT_NAME
+    constexpr const char* projectName = SF_PROJECT_NAME;
+#else
+    constexpr const char* projectName = "<unknown name>";
+#endif
+
+#ifdef SF_PROJECT_VERSION
+    constexpr const char* projectVersion = SF_PROJECT_VERSION;
+#else
+    constexpr const char* projectVersion = "<unknown version>";
+#endif
+
 int main()
 {
-    std::cout << "C++: main() executed\n";
+    std::cout << "C++: " << projectName << " " << projectVersion << " started\n";
     // noop, required for emscripten, so that *.js and *.wasm files get
     // generated for library configured as dependency see:
     // https://stackoverflow.com/questions/34234446/cmake-is-it-possible-to-build-an-executable-from-only-static-libraries-and-no-s
